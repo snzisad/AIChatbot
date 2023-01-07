e4b62b9f3b75203618ae9b87d1814b0c741f7b08
snzisad <snzisad@gmail.com>
2023-01-07 12:41:54 +0100

    static file fixed

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,5 @@ python manage.py makemigrations --no-input
 python manage.py migrate --no-input
 python manage.py collectstatic --no-input
 
+# python manage.py runserver 0.0.0.0:8000
 gunicorn backend.wsgi:application --bind 0.0.0.0:8000
